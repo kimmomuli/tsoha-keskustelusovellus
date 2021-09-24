7cc81b00e01b9d30d823cd48df1e014fa4d072e3
Kimmo Muli <kimmo.muli@hotmail.com>
2021-09-24 16:55:32 +0300

    heroku

--- db.py
+++ db.py
@@ -2,9 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from app import app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 #Vaihda alempi kun pusket herokuun. Paikallisesti pitää käyttää ylempää
-#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 
 #Virheilmoituksen poisto
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
